0061e689520ff8bc4cf0a38942f01720fdb6ab1f
Colm Morgan <colm.a.morgan@gmail.com>
2013-06-08 15:47:20 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Global Dev - Singles365
+
+This project is part of an application process for Global Personals.
+
+This brief example should demonstrate my skills  in HTML/CSS markup while introducing some JavaScript functionality.
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="ie ie6 lte9 lte8 lte7" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><![endif]-->
+<!--[if IE 7]><html class="ie ie7 lte9 lte8 lte7" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><![endif]-->
+<!--[if IE 8]><html class="ie ie8 lte9 lte8" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie ie9 lte9" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><![endif]-->
+<!--[if gt IE 9]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><![endif]-->
+<!--[if !IE]><!--><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><!--<![endif]-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Global Personals :: Singles365.com</title>
+</head>
+
+<body>
+</body>
+</html>
