28be8d4ed85a33c9df404c75871a26fb9587d333
Colm Morgan <colm.a.morgan@gmail.com>
2013-06-10 02:05:33 +0100

    updated final edits and README

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 This project is part of an application process for Global Personals.
 
 This brief example should demonstrate my skills  in HTML/CSS markup while introducing some JavaScript functionality.
+
+## Intentions
+
+My main intentions are to firstly, and foremost, make a responsive prototype.
+
+My next priority is to automate the input of data using a JSON string provided, which would then allow for a content structure to be implemented appropriately. This method of data input would also allow me to read through the content and, upon the user's request in the browser, sort the results as they wish, using options provided.
+
+## Successes
+
+I successfully completed a responsive prototype, alternating between several views of how the results are displayed in a grid layout, or a more detailed view.
+
+I automated the input of data in an effort to reduce the amount of hardcoded HTML, and code in general.
+
+## Failures
+
+Had I more time, I would have been able to implement auto-sort functionality into the drop-down menu above the results, where the user would select their preference, and have their re-ordered list displayed.
+
+This method would have involved using the `usort()` function, but to also include a script to re-order the search results without refreshing or reloading the page.
--- index.php
+++ index.php
@@ -13,9 +13,12 @@
 	<title>Global Personals :: Singles365.com</title>
 
 	<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+	<!-- styles.CSS (minified) is pre-compiled from styles.LESS via WinLESS app for Windows -->
 	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<!-- Using my current personal license, I have used an icon font as a text replacement technique -->
 	<link rel="stylesheet" type="text/css" href="assets/css/ss-standard.css" />
 
+	<!-- Modernizr - used to aid in legacy browser versions -->
 	<script type="text/javascript" src="assets/js/modernizr.min.js"></script>
 </head>
 
@@ -28,6 +31,11 @@
 	</header>
 
 	<div class="content">
+		<!--
+			The navigation layout below is to allow for a collapsed, 
+			mobile-friendly menu to be implemented.
+			More notes for this included in styles.css
+		-->
 		<nav class="top-nav" role="navigation">
 			<div class="openers">
 				<a class="opener opener--open" href="#mainmenu">MENU</a>
@@ -37,6 +45,14 @@
 					<a class="opener opener--close" href="#0">CLOSE MENU</a>
 				</div>
 
+				<!--
+					Menu split into several modules to allow for future implementation 
+					of a multi-tiered, collapsable, mobile-friendly menu.
+
+					HTML Comments used to nullify white space when using 
+					display: inline; and display: inline-block;
+					This technique has been used throughout.
+				-->
 				<ul class="menu nav--global">
 					<li class="nav--global-options"><a href="#">Home</a></li><!--
 				 --><li class="nav--global-options"><a href="#">Search</a></li><!--
@@ -64,14 +80,20 @@
 			
 			<div class="search-meta">
 				<div class="members-count">
+					<!-- Typically, this would not be hard-coded, but called via a feed of total, or currently online, users -->
 					<p><span id="members-count-number" class="members-count-number">31,000</span> members</p>
 				</div><!--
 			 --><div class="view-options">
+					<!-- Case in point: Symbol font by SymbolSet (Symbolset Standard) -->
 					<ul><li class="ss-icon"><span id="view-grid" class="view-option active">grid</span></li><!--
 					 --><li class="ss-icon"><span id="view-rows" class="view-option">rows</span></li>
 					</ul>
 				</div><!--
 			 --><div class="view-sort">
+				<!--
+					the options listed in this drop-down menu would be scripted to 
+					automatically sort, before displaying, the data from the JSON results.
+				-->
 					Sort by<select class="view-sort-options">
 								<option value="distance">distance</option>
 								<option value="distance">youngest</option>
@@ -85,9 +107,15 @@
 			$json_a = file_get_contents('assets/json/results.json');
 			$json_results = json_decode($json_a);
 
-			?><div class="search-results view-grid" id="search-results"><?php
+			?><!--
+				The search results layout is affected by the view-options 
+				mentioned above, and controlled by a snippet of JavaScript
+			--><div class="search-results view-grid" id="search-results"><?php
 
 			if ( count($json_results) > 0 ) {
+				/**
+				 * Gathering the required data from the JSON string
+				 */
 				foreach ($json_results->members as $member) {
 					$member_photo_url = $member->photo;
 					$member_photo_count = $member->photo_count;
@@ -100,13 +128,17 @@
 
 			?><article class="member">
 					<div class="member--photo"><?php
+					// Initial check to ensure an image exists in $member->photo
 					if ($member_photo_count !== 0) { ?>
 						<img class="member--profile-pic" src="<?=$member_photo_url;?>"><?php
+						// Further check to see if multiple images exist, rather than just one
+						// If so, a link/notification is displayed hinting at more photos available
 						if ($member_photo_count > 1){
 							?><div class="member--total-photos ss-camera"><?=$member_photo_count?></div><?php
 						}
 					} ?>
 					<?php if ($member_photo_count == 0) { ?>
+					<!-- Self-explanatory, only displayed when no photos have been submitted by the site member -->
 					<p class="no-photo">No photos available</p>
 				<?php }
 				?></div><!--
@@ -114,22 +146,25 @@
 						<p><span class="member--name"><?=$member_firstname?></span>, <span class="member--age"><?=$member_age?></span></p>
 						<p><span class="member--location"><?=$member_location?></span></p>
 
+						<!-- More meta data for each site member is displayed but only when the view-rows option is in use -->
 						<div class="member--meta--more">
 							<p class="member--profile"><?=$member_profile?></p>
 							<p class="member--interests"><span class="data-head">Interests:</span> <?php
+								// Each of the site member's interests are listed in readable sentence form, separated accordingly
 								$sep = '';
 								foreach ( $member_interests as $interest ) {
 									echo $sep.$interest;
 									$sep = ', ';
 								}
 							?></p>
+							<!-- A notification to the current viewer as to when the site member was last online -->
 							<p class="member--online-status"><span class="data-head">Last online:</span> <?=$member_online_status?></p>
-							</ul>
 						</div>
 					</div>
 				</article><?php
 				}
 			} else { ?>
+				<!-- Fallback for when no results are found -->
 				<p>Unfortunately, there are no results for that search. Please try again.</p>
 			<?php } ?>
 			</div>
@@ -138,6 +173,15 @@
 </div>
 
 <script type="text/javascript">
+/**
+ * Simple piece of JavaScript used rather than jQuery (or equivalent)
+ * as pure JavaScript will run faster and not hold up any load times
+ * 
+ * If JavaScript is disabled, the viewer can still see basic information 
+ * for each member, with further options to open up the member's 
+ * dedicated profile page
+ * 
+ */
 document.getElementById('view-rows').onclick = function(){
 	this.className = this.className+"view-option active";
 	document.getElementById('view-grid').className = "view-option";
